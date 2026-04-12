04811b8219a03c0e3e48773b4b4cbd4ae8a59817
yasmineBouziri <ysbouziri@gmail.com>
2026-04-12 21:49:44 +0100

    correcting the path to session in get_client.php in service_admin

--- web pages/service admin/get_clients.php
+++ web pages/service admin/get_clients.php
@@ -1,6 +1,6 @@
 <?php
     require_once '../../config/database.php';
-    require_once '../../config/session.php';
+    require_once '../../config/session_check.php';
 
     header('Content-Type: application/json');
 
